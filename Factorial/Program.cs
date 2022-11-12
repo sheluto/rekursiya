4a49cfb9330271063a0274a501bfc952f9acff27
«Sheluto» <sheluto@gmail.com>
2022-11-13 00:39:54 +0300

    Пример рекурсии в вычислении факториала

---
+++ Factorial/Program.cs
@@ -0,0 +1,18 @@
+﻿// Рекурсия! вычисляем факториал
+
+double Factorial (int n) 
+{
+    //1! = 1
+    //0! = 1
+    if(n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+
+//Console.WriteLine(Factorial(5)); // 1*2*3 = 6
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+
