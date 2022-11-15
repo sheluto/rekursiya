cb5e63e5ca195761ff33e14463bc9160f54985ca
«Sheluto» <sheluto@gmail.com>
2022-11-15 21:43:44 +0300

    Поворачиваем одномерный массив

---
+++ Seminar_001/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 39: Напишите программу, которая перевернёт одномерный массив 
+//(последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+
+int[] array = GetArray(10,0,10);
+PrintArray(array);
+Console.WriteLine();
+//ReversArray1(array);
+//PrintArray(array);
+int[] reversArray = ReversArray2(array);
+Console.WriteLine();
+PrintArray(reversArray);
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for(int i=0; i<size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue);
+    }
+    return res;
+}
+
+void ReversArray1(int[] inArray)
+{
+    for(int i=0; i<inArray.Length/2; i++)
+    {
+        int k = inArray[i];
+        inArray[i] = inArray[inArray.Length-1 -i];
+        inArray[inArray.Length-1 -i] = k;
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for(int i=0; i<arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int[] ReversArray2(int[] inArray)
+{
+    int[] result = new int[inArray.Length];
+    for(int i=0; i<inArray.Length; i++)
+    {
+        result[i] = inArray[inArray.Length -1 -i];
+    }
+    return result;
+}
